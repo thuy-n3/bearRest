12b2c7ba2bc0acb7a812092765f909c23a698cc7
Thuy Nguyen <ngtan3@gmail.com>
2016-10-07 23:53:45 -0500

    creating and saving bears

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json());
 //set out port 
 var port = process.env.PORT || 8080; 
 
+
 //ROUTES FOR API
 //==============================================
 
@@ -35,7 +36,30 @@ router.get('/', function(req, res){
 	res.json({ message: 'hooray! welcome to api!! '});
 });
 
-//more routes here 
+
+//on routes end in '/bears'
+//-----------------------------------------------
+router.route('/bears')
+
+	//create a bear (POST http://localhost:8080/api/bears)
+	.post(function(req, res){
+
+		var bear = new Bear();		 //create a new instance of Bear model 
+		bear.name = req.body.name;	//set the bears name (from the request)
+
+
+		//save bear and check for error 
+		bear.save(function(err){
+			if(err)
+				res.send(err);
+
+			res.json({ message: 'Bear created!' });
+
+		});
+
+	});
+
+
 
 
 
